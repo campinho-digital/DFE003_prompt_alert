882facd5e6884945abba58904f5880d19ed95844
LucianaChamma <107868796+LucianaChamma@users.noreply.github.com>
2023-05-07 19:06:16 -0300

    desafio valor conta

--- src/main.js
+++ src/main.js
@@ -3,3 +3,21 @@ const entrarNumero = prompt("Entre um número");
 const dobroNumero = entrarNumero*2
 
 alert("O dobro do número digitado é " + dobroNumero + ".");
+
+
+
+
+const entrarValorConta = prompt("Digite o valor da sua conta:");
+
+const percentual = 0.10;
+
+const totalPercent = entrarValorConta * percentual;
+
+const totalConta = parseInt(entrarValorConta)+ parseInt(totalPercent);
+
+alert("O valor total da conta acrescido da taxa de serviço é " + totalConta);
+
+
+
+
+
